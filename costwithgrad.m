595a5a72467ff1347b61b8ae50bd395cf3d4d195
Henry.L <1401584+hrluo@users.noreply.github.com>
2019-08-08 11:54:52 -0400

    with gradient

--- costwithgrad.m
+++ costwithgrad.m
@@ -1,11 +1,5 @@
 function [f,g] = costwithgrad(x)
 % Calculate objective f
-delta_Matrix=dlmread('D:/icerm-tda-2019-08/coboundary.txt');
-%
-fc=dlmread('D:/icerm-tda-2019-08/l2cocycle.txt');
-z0=zeros(size(delta_Matrix,1),1);
-delta_Matrix=transpose(delta_Matrix);
-
 %f = 100*(x(2) - x(1)^2)^2 + (1-x(1))^2;
 f = sum(abs(fc-delta_Matrix*x) );
 
